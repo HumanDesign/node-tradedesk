d4bf56c105dbfed8ffa9b890df41b712559197fe
sthoma <sthoma4@gmail.com>
2015-05-26 14:08:00 -0600

    Update the example file.

--- example.js
+++ example.js
@@ -1,11 +1,18 @@
 var Tradedesk = require('./lib/tradedesk');
-var client = Tradedesk({
-                      login: 'test',
-                      password: 'swordfish'
-                      });
+var client = Tradedesk({token: 'thetoken'});
 
-client.get('/supplyvendor/query/facets', function(error, x ,res) {
+client.get('adgroup/query/facets', function(error, x, res) {
   if (error) throw error;
   console.log(x);
-  console.log(res);
 });
+
+client.get('/overview/partner', {partnerid: 'dly0gyd'}, function(error, x ,res) {
+  if (error) throw error;
+  console.log(x);
+});
+
+client.post('contract/query/partner/available', {partnerid: 'dly0gyd', PageStartIndex: 0, PageSize: 0}, function(error, x ,res) {
+  if (error) throw error;
+  console.log(x);
+});
+

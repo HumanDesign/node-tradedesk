cc6057a6acdd5ec7983fd63cd515a5648efc5bc9
sthoma <sthoma4@gmail.com>
2015-05-27 10:34:26 -0600

    Update auth flow.

--- example.js
+++ example.js
@@ -1,18 +1,15 @@
 var Tradedesk = require('./lib/tradedesk');
-var client = Tradedesk({token: 'thetoken'});
+var client = Tradedesk();
 
-client.get('adgroup/query/facets', function(error, x, res) {
-  if (error) throw error;
-  console.log(x);
+client.getAuthToken('login', 'pass', function(token) {
+  client.setAuthToken(token);
+  client.get('adgroup/query/facets', function(error, x, res) {
+    if (error) throw error;
+    console.log(x);
+  });
+
+  client.post('contract/query/partner/available', {partnerid: 'XXXXX', PageStartIndex: 0, PageSize: 0}, function(error, x ,res) {
+    if (error) throw error;
+    console.log(x);
+  });
 });
-
-client.get('/overview/partner', {partnerid: 'dly0gyd'}, function(error, x ,res) {
-  if (error) throw error;
-  console.log(x);
-});
-
-client.post('contract/query/partner/available', {partnerid: 'dly0gyd', PageStartIndex: 0, PageSize: 0}, function(error, x ,res) {
-  if (error) throw error;
-  console.log(x);
-});
-
